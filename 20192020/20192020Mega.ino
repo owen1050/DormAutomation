c5e07bbb0f2ae1e9cf701578d8ad0a9b48273194
sadmin <sadmin@bserver.fios-router.home>
2019-08-09 18:12:39 +0000

    new file

---
+++ 20192020/20192020Mega.ino
@@ -0,0 +1,10 @@
+//this will be the code for mega dorm automation 20192020
+
+void setup()
+{
+}
+
+void main()
+{
+}
+
--- 20192020Mega.ino
+++
@@ -1,6 +0,0 @@
-//this is the dorm automation for 20192020
-
-void setup(){
-}
-void loop(){
-}
