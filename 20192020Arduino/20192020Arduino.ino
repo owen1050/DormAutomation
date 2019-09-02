cea631cf4a9bf6db25429a97fb6fe250940ce8df
owen1050 <owen1050@gmail.com>
2019-09-01 22:58:38 -0400

    update - mega replies to main light 1 with 0

--- 20192020Arduino/20192020Arduino.ino
+++ 20192020Arduino/20192020Arduino.ino
@@ -7,19 +7,14 @@ int serverPort = 23654;
 EthernetClient client;
 boolean good = true;
 String gVars[] = {"blindsMoveAllUp", "0", "blindMoveAllDown", "0", "blindMovePUp", "0", "blindMovePDown", "0", "mainLightOn", "0", "mainLightOff", "0", "hallLightOn", "0", "hallLightOff", "0", "projectorOn", "0", "projectorOff", "0", "tvOn", "0", "tvOff", "0", "tvInComp", "0", "tvInChrome", "0", "projInComp", "0", "projInChrome", "0", "speakerMute", "0", "speakerInProj", "0", "speeakerInTV", "0", "speakerInPhono", "0", "speakerInChrome", "0", "speakerVolumeUp", "0", "speakerVolumeDown", "0"};
-
+ //                 0                         2                         4                       6                   8                         10                12                    14              16                      18                20    
 void setup() {
   Serial.begin(9600);
-  String a = "gahr";
-  int p = 1;
-  p = a.toInt();
-  Serial.println(p);
   pinMode(4,OUTPUT);
   digitalWrite(4,HIGH);
   Serial.print(F("Starting ethernet..."));
   if(!Ethernet.begin(mac)) Serial.println(F("failed"));
   else Serial.println(Ethernet.localIP());
-
   delay(2000);
   Serial.println(F("Ready"));
 }
@@ -33,24 +28,31 @@ void loop() {
   if(check < 0)
   {
     good = true;
-    Serial.println("no no_change");
     for(int i = 0; i <= 22 && good; i++)
     {
       int ti = str.indexOf(gVars[2*i]);
       if(ti<0)
       {
         good = false;
-      }
-      
-    }
-    Serial.println(good);
+      }      
+    }    
   }
   if(good)
   {
     updateGvars(str);
   }
+  if(gVars[9].toInt()==1)
+  {
+    Serial.println("TURN MAIN LIGHT ON");
+    postPage("set:mainLightOn=0;");
+  }
+  if(gVars[11].toInt()==1)
+  {
+    Serial.println("TURN MAIN LIGHT OFF");
+    postPage("set:mainLightOff=0;");
+  }
   
-  delay(50);
+  delay(20);
 }
 
 String postPage(char* thisData)
@@ -112,9 +114,9 @@ void updateGvars(String nVars){
     int sVI = nVars.indexOf("=", lowI);
     int eVI = nVars.indexOf("!", sVI);
     gVars[i+1] = nVars.substring(sVI+1, eVI);
-    Serial.print("var=");
-    Serial.print(gVars[i]);
-    Serial.print(":");
-    Serial.println(gVars[i+1]);
+    //Serial.print("var=");
+    //Serial.print(gVars[i]);
+    //Serial.print(":");
+    //Serial.println(gVars[i+1]);
   }
 }
